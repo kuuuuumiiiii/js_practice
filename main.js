c56336b4b0db4f4c40e52bdcd90121931f98c943
kuuuuumiiiii <kuuuuumiiiii@users.noreply.github.com>
2023-07-10 22:11:29 +0900

    javascriptの基本209

---
+++ main.js
@@ -0,0 +1,38 @@
+'use strict';
+
+// console.log(10 +3);
+
+// console.log(10 -3);
+
+// console.log(10 * 3);
+
+// console.log(10 / 3);
+
+// console.log(10 % 3);
+
+// let x;
+// console.log(x);
+// x = 1;
+// console.log(x);
+// x = 2;
+// console.log(x);
+// let x;
+
+// let y = 10;
+// console.log(y);
+// y = 11;
+// console.log(y);
+
+// let str = 'Hello JavaScript'
+// let str = "Hello JavaScript"
+// console.log(str)
+
+// let i, j, k;
+
+
+// const a = 1;
+// console.log(a);
+
+// const prefecture = 'Tokyo';
+// console.log(prefecture);
+
